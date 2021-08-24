311fb33524824a9a65fadd92e0a1e56c33b6ed52
krlmari <krylova_m_v@inbox.ru>
2021-08-25 01:00:30 +0300

    add .prettierrc

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const express = require("express");
+const express = require('express');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT, () => {});
