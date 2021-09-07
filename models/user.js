f5447eb8d9bac9d0b1f5cb04e76fe7a9f51c3714
krlmari <krylova_m_v@inbox.ru>
2021-09-07 21:37:23 +0300

    update user

--- models/user.js
+++ models/user.js
@@ -4,16 +4,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
