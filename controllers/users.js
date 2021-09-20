1a734df130e16f83f0b9d3bc979d3b19497fd7a9
krlmari <krylova_m_v@inbox.ru>
2021-09-20 23:34:10 +0300

    add catch

--- controllers/users.js
+++ controllers/users.js
@@ -2,19 +2,21 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const getUsers = (req, res) => {
+const getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(404).send({ message: 'Запрашиваемые пользователи не найдены' }));
+    .catch(() => res.status(404).send({ message: 'Запрашиваемые пользователи не найдены' }))
+    .catch(next);
 };
 
-const getUserId = (req, res) => {
+const getUserId = (req, res, next) => {
   User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(404).send({ message: 'Запрашиваемый пользователь не найден' }));
+    .catch(() => res.status(404).send({ message: 'Запрашиваемый пользователь не найден' }))
+    .catch(next);
 };
 
-const createUser = (req, res) => {
+const createUser = (req, res, next) => {
   const { name, about, avatar, email } = req.body;
   bcrypt
     .hash(req.body.password, 10)
@@ -30,26 +32,29 @@ const createUser = (req, res) => {
     // Вернём записанные в базу данные
     .then((user) => res.send({ data: user }))
     // Данные не записались, вернём ошибку
-    .catch(() => res.status(400).send({ message: 'Некорректные данные' }));
+    .catch(() => res.status(400).send({ message: 'Некорректные данные' }))
+    .catch(next);
 };
 
-const updateUser = (req, res) => {
+const updateUser = (req, res, next) => {
   const { name, about } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { name, about })
     .then((newUser) => res.send({ data: newUser }))
-    .catch(() => res.status(400).send({ message: 'Некорректные данные' }));
+    .catch(() => res.status(400).send({ message: 'Некорректные данные' }))
+    .catch(next);
 };
 
-const updateAvatar = (req, res) => {
+const updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { avatar })
     .then((newAvatar) => res.send({ data: newAvatar }))
-    .catch(() => res.status(400).send({ message: 'Некорректные данные' }));
+    .catch(() => res.status(400).send({ message: 'Некорректные данные' }))
+    .catch(next);
 };
 
-const login = (req, res) => {
+const login = (req, res, next) => {
   const { email, password } = req.body;
 
   User.findUserByCredentials(email, password)
@@ -60,7 +65,8 @@ const login = (req, res) => {
     })
     .catch((err) => {
       res.status(401).send({ message: err.message });
-    });
+    })
+    .catch(next);
 };
 
 const getUserInfo = (req, res, next) => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,30 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-const handleAuthError = (res) => {
-  res.status(401).send({ message: 'Необходима авторизация' });
-};
-
-const extractBearerToken = (header) => header.replace('Bearer ', '');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res);
-  }
-
-  const token = extractBearerToken(authorization);
+  const token = req.cookies.jwt;
   let payload;
-
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
   } catch (err) {
-    return handleAuthError(res);
+    res.status(401).send({ message: 'Необходима авторизация' });
   }
-
   req.user = payload;
 
-  return next();
+  next();
 };
 
 module.exports = auth;
