eb1ec48a516425fe4db4de44fd7dd2f51bf9169b
krlmari <krylova_m_v@inbox.ru>
2021-09-20 22:54:46 +0300

    add select

--- models/user.js
+++ models/user.js
@@ -34,18 +34,20 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = (email, password) =>
-  this.findOne({ email }).then((user) => {
-    if (!user) {
-      return Promise.reject(new Error('Неправильные почта или пароль'));
-    }
-
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
       }
 
-      return user;
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error('Неправильные почта или пароль'));
+        }
+
+        return user;
+      });
     });
-  });
 
 module.exports = mongoose.model('user', userSchema);
